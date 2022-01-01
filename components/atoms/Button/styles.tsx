8e320308e8fe0a8e052712d53068e8d9e542f119
TahmidU <tahmid.uddin.dev@gmail.com>
2022-01-01 21:45:53 +0000

    ThemesProvider now applying in styled-components

--- components/atoms/Button/styles.tsx
+++ components/atoms/Button/styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ButtonStyle = styled.button`
-  border: 1px solid red;
+  border: 10px solid ${({ theme }) => theme.red};
   border-radius: 10px;
 `;
 ButtonStyle.displayName = "ButtonStyle";
--- resources/themes.tsx
+++ resources/themes.tsx
@@ -1,18 +1,35 @@
 import { DefaultTheme } from "styled-components";
 import { Theme } from "../types/context";
 
+const colours = {
+  almostBlack: "#261B00",
+  red: "#EA2321",
+  crimson: "#812626",
+  white: "#FFFFFF",
+  almostWhite: "#e8e8e8",
+  black: "#000000",
+};
+
 const themes: { [key: string]: Partial<DefaultTheme> } = {
   light: {
     is: "light",
-    main: "white",
-    text: "black",
-    accent: "green",
-    reverseMain: "black",
+    main: colours.white,
+    text: colours.almostBlack,
+    accent: colours.red,
+    reverseMain: colours.almostBlack,
+  },
+  dark: {
+    is: "dark",
+    main: colours.almostBlack,
+    text: colours.almostWhite,
+    accent: colours.red,
+    reverseMain: colours.almostWhite,
   },
 };
 
 export default function getTheme(themeSelect: Theme): DefaultTheme {
   return {
+    ...colours,
     ...themes[themeSelect],
   };
 }
