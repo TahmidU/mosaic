19ecf70bf5453990b112b330190c962656cc4c79
TahmidU <tahmid.uddin.dev@gmail.com>
2022-02-16 21:26:33 +0000

    StepStatus component created

---
+++ components/organisms/Carousel/StepStatus/StepStatus.stories.tsx
@@ -0,0 +1,11 @@
+import StepStatus from ".";
+import { storyTitle } from "../../../../resources/utils/story";
+
+const payload = {
+  title: storyTitle("ATOMS", "StepStatus"),
+  component: StepStatus,
+};
+export default payload;
+
+export const Basic = (args: any) => <StepStatus {...args} />;
+Basic.args = {};
---
+++ components/organisms/Carousel/StepStatus/StepStatus.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface StepStatusProps {}
+
+export default function StepStatus({}: StepStatusProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
