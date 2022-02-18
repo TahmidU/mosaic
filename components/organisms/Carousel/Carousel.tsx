1e16bd1c5786800ef36e3d2e81faf48f0c37195e
TahmidU <tahmid.uddin.dev@gmail.com>
2022-02-18 22:39:37 +0000

    More styling added to Carousel component

--- components/organisms/Carousel/Carousel.tsx
+++ components/organisms/Carousel/Carousel.tsx
@@ -1,9 +1,23 @@
+import { CarouselContainer, Container, ImageStyle, TextStyle } from "./styles";
+
 import { ReactElement } from "react";
+import SlideButton from "./SlideButton";
 
 export default function Carousel(): ReactElement {
   return (
-    <div>
-      <div>Carousel</div>
-    </div>
+    <Container>
+      <SlideButton variant="left" />
+      <CarouselContainer>
+        <ImageStyle src="/../../../public/logo/logo.png" layout="fill" />
+        <TextStyle>
+          <p>Title</p>
+          <p>
+            Description, Excepteur velit in proident eu cillum qui reprehenderit
+            dolor est.
+          </p>
+        </TextStyle>
+      </CarouselContainer>
+      <SlideButton variant="right" />
+    </Container>
   );
 }
--- components/organisms/Carousel/styles.tsx
+++ components/organisms/Carousel/styles.tsx
@@ -1,1 +1,35 @@
+import Image from "next/image";
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: auto;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+Container.displayName = "Container";
+
+export const CarouselContainer = styled.div`
+  width: 80%;
+  height: 862px;
+  position: relative;
+  border-radius: 12px;
+`;
+CarouselContainer.displayName = "CarouselContainer";
+
+export const ImageStyle = styled(Image)``;
+ImageStyle.displayName = "ImageStyle";
+
+export const TextStyle = styled.div`
+  position: absolute;
+  z-index: 1;
+
+  > p:first-child {
+  }
+
+  > p:nth-child(2) {
+  }
+`;
+TextStyle.displayName = "TextStyle";
