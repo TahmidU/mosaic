161ea88a2dca847a922f43077c2dc244f3e5fb8c
TahmidU <tahmid.uddin.dev@gmail.com>
2022-03-05 16:52:16 +0000

    Storybook import path config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   staticDirs: ["../public"],
   stories: [
@@ -10,4 +12,23 @@ module.exports = {
     "storybook-addon-styled-component-theme/dist/preset",
   ],
   framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      resolve: {
+        alias: {
+          atoms: path.resolve(__dirname, "../components/atoms"),
+          molecules: path.resolve(__dirname, "../components/molecules"),
+          organisms: path.resolve(__dirname, "../components/organisms"),
+          pages: path.resolve(__dirname, "../components/pages"),
+          context: path.resolve(__dirname, "../context"),
+          hooks: path.resolve(__dirname, "../hooks"),
+          icons: path.resolve(__dirname, "../icons"),
+          resources: path.resolve(__dirname, "../resources"),
+          translations: path.resolve(__dirname, "../translations"),
+          types: path.resolve(__dirname, "../types"),
+        },
+      },
+    });
+    return config;
+  },
 };
