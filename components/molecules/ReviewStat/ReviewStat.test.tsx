0453ede9dd43c29d65295c40f2e9ec638b6aa754
TahmidU <tahmid.uddin.dev@gmail.com>
2022-03-12 14:55:32 +0000

    Additional tests added to ReviewStat.test.tsx

--- components/molecules/ReviewStat/ReviewStat.test.tsx
+++ components/molecules/ReviewStat/ReviewStat.test.tsx
@@ -7,11 +7,30 @@ afterEach(cleanup);
 
 describe("ReviewStat", () => {
   test("Review 0%", () => {
+    // Given When
     const review = 0;
-    const { getByText } = render(
-      <ReviewStat percentage={review} className="" />
-    );
+    const { getByText } = render(<ReviewStat percentage={review} />);
 
-    expect(getByText(`${review}%`)).toBeDefined();
+    // Then
+    getByText(`${review}%`);
+  });
+
+  test("Review 90% Non-wait", () => {
+    // Given When
+    const review = 90;
+    const { queryByText } = render(<ReviewStat percentage={review} />);
+
+    // Then
+    expect(queryByText(`${review}%`)).toBe(null);
+  });
+
+  test("Review 90%", async () => {
+    // Given When
+    const review = 90;
+    const { getByText } = render(<ReviewStat percentage={review} />);
+
+    // Then
+    await new Promise((r) => setTimeout(r, 2000));
+    getByText(`${review}%`);
   });
 });
