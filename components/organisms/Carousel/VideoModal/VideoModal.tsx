49bd6546078fae383f23ca3fd30d342894d21f29
TahmidU <tahmid.uddin.dev@gmail.com>
2022-04-23 13:48:16 +0100

    Additional styling for VideoModal component

--- components/organisms/Carousel/VideoModal/VideoModal.tsx
+++ components/organisms/Carousel/VideoModal/VideoModal.tsx
@@ -1,11 +1,22 @@
+import { Container, Frame, Overlay } from "./styles";
+
 import { ReactElement } from "react";
 
 interface VideoModalProps {}
 
 export default function VideoModal({}: VideoModalProps): ReactElement {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <Overlay />
+      <Container>
+        <Frame
+          src="https://www.youtube-nocookie.com/embed/JfVOs4VSpmA"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; rel=0; showinfo=0; autohide=1; clipboard-write; encrypted-media; gyroscope;"
+          allowFullScreen
+        />
+      </Container>
+    </>
   );
 }
--- components/organisms/Carousel/VideoModal/styles.tsx
+++ components/organisms/Carousel/VideoModal/styles.tsx
@@ -1,1 +1,31 @@
 import styled from "styled-components";
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: ${({ theme }) => theme.cBlack.alpha(0.7).toString()};
+  width: 100%;
+  height: 100%;
+  z-index: 99999;
+`;
+Overlay.displayName = "Overlay";
+
+export const Container = styled.div`
+  position: fixed;
+  top: 5%;
+  right: 15%;
+  width: 70%;
+  height: 90%;
+  background-color: white;
+  z-index: 99999;
+`;
+Container.displayName = "Container";
+
+export const Frame = styled.iframe`
+  width: 100%;
+  height: 100%;
+`;
+Frame.displayName = "Frame";
