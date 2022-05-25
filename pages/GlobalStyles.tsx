d92d4f2ad45fbf93daa52e81cde4653cae0d3bbc
TahmidU <tahmid.uddin.dev@gmail.com>
2022-05-25 22:36:00 +0100

    react-scroll added. Global styling changes, replacing react-custom-scrollbars

---
+++ pages/GlobalStyles.tsx
@@ -0,0 +1,25 @@
+import { DefaultTheme, createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
+  
+  html::-webkit-scrollbar{
+    width: 0.625rem;
+  }
+
+  html::-webkit-scrollbar-thumb{
+    background-color: ${({ theme }) =>
+      theme.cAlmostBlack.alpha(0.8).toString()};
+    border-radius: 1rem;
+
+    :hover{
+        background-color: ${({ theme }) =>
+          theme.cAlmostBlack.alpha(0.85).toString()}; 
+    }
+  }
+
+  html::-webkit-scrollbar-track{
+    background-color: ${({ theme }) => theme.cAlmostWhite};
+  }
+
+`;
+GlobalStyle.displayName = "GlobalStyle";
