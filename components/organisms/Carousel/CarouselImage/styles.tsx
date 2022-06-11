ec7d02159d62f426e41d5bca458d3d9671d91a5d
TahmidU <tahmid.uddin.dev@gmail.com>
2022-06-11 13:06:11 +0100

    Styling improvements

--- components/organisms/Carousel/CarouselImage/styles.tsx
+++ components/organisms/Carousel/CarouselImage/styles.tsx
@@ -7,7 +7,6 @@ export const LinearGradient = styled.div`
   width: 100%;
   height: 100%;
   z-index: 2;
-  border-radius: 48px;
 
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   background: linear-gradient(
--- components/organisms/Carousel/MobileCarousel/styles.tsx
+++ components/organisms/Carousel/MobileCarousel/styles.tsx
@@ -6,6 +6,7 @@ export const Container = styled.div`
   width: 100%;
   height: 55.7vw;
   position: relative;
+  background-color: ${({ theme }) => theme.almostBlack};
 `;
 Container.displayName = "Container";
 
