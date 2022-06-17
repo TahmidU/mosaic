6cf96751b7a55243499dd9e9b5b85c15530080b6
TahmidU <tahmid.uddin.dev@gmail.com>
2022-06-17 23:09:43 +0100

    Styling update

--- components/atoms/SelectorComponent/SelectorComponent.tsx
+++ components/atoms/SelectorComponent/SelectorComponent.tsx
@@ -4,13 +4,16 @@ import { ReactElement } from "react";
 
 interface ISelectorComponent {
   options: string[];
+  className?: string;
 }
 
+// ! Create a custom select
 export default function SelectorComponent({
   options,
+  className,
 }: ISelectorComponent): ReactElement {
   return (
-    <Container>
+    <Container className={className}>
       {options.map((_option, key) => {
         return (
           <Option key={key} value={_option}>
--- components/atoms/SelectorComponent/styles.tsx
+++ components/atoms/SelectorComponent/styles.tsx
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
-export const Container = styled.select``;
+export const Container = styled.select`
+  border-radius: 1.25rem;
+  padding: 0.625rem;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background: transparent;
+  background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='32' viewBox='0 0 24 24' width='32' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+  background-repeat: no-repeat;
+  background-position-x: 100%;
+  background-position-y: 5px;
+  ${({ theme }) => theme.fonts.main.big};
+  width: 168px;
+  cursor: pointer;
+`;
 Container.displayName = "Container";
 
 export const Option = styled.option``;
