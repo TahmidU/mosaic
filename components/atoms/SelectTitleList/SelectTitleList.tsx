871f548a14984f665f38a29094f96e587255c6df
TahmidU <tahmid.uddin.dev@gmail.com>
2022-06-19 20:42:25 +0100

    SelectTitleList component complete

--- components/atoms/SelectTitleList/SelectTitleList.tsx
+++ components/atoms/SelectTitleList/SelectTitleList.tsx
@@ -1,11 +1,34 @@
-import { ReactElement } from "react";
+import { Container, Option } from "./styles";
+import { ReactElement, useState } from "react";
 
-interface ISelectTitleListProps {}
+interface ISelectTitleListProps<T> {
+  options: T[];
+  onChange?: (selected: T) => void;
+}
 
-export default function SelectTitleList({}: ISelectTitleListProps): ReactElement {
+export default function SelectTitleList<T>({
+  options,
+  onChange = () => {},
+}: ISelectTitleListProps<T>): ReactElement {
+  const [currIndex, setIndex] = useState(0);
+
+  const onSelect = (selected: T, index: number) => {
+    onChange(selected), setIndex(index);
+  };
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <Container>
+      {options.map((_option, _index) => {
+        return (
+          <Option
+            key={_index}
+            highlight={_index === currIndex}
+            onClick={() => onSelect(_option, _index)}
+          >
+            {_option}
+          </Option>
+        );
+      })}
+    </Container>
   );
 }
