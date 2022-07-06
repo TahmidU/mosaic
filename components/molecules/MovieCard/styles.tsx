147357818c79e832444a91b5823b5ad11be4fa68
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-06 16:06:41 +0100

    MovieCard style.tsx clean-up

--- components/molecules/MovieCard/styles.tsx
+++ components/molecules/MovieCard/styles.tsx
@@ -1,5 +1,4 @@
 import Card from "components/atoms/Card";
-import Image from "next/image";
 import styled from "styled-components";
 
 export const CardStyle = styled(Card)`
@@ -28,46 +27,3 @@ export const CardStyle = styled(Card)`
   }
 `;
 CardStyle.displayName = "CardStyle";
-
-export const Wrapper = styled.div`
-  width: 100%;
-  max-width: 150px;
-  height: 100%;
-  max-height: 225px;
-  position: relative;
-`;
-Wrapper.displayName = "Wrapper";
-
-export const MovieImage = styled(Image)`
-  width: 100%;
-  height: 100%;
-  border-radius: 14px;
-`;
-MovieImage.displayName = "MovieImage";
-
-export const SubSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 0.75em;
-  text-align: center;
-
-  > span:first-child {
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: normal;
-    ${({ theme }) => theme.fonts.tertiary.regular};
-    font-weight: 600;
-    margin-bottom: 0.1em;
-  }
-
-  > span:last-child {
-    ${({ theme }) => theme.fonts.tertiary.small};
-    font-weight: 100;
-    color: ${({ theme }) => theme.cAlmostBlack.alpha(0.7)};
-  }
-`;
-SubSection.displayName = "SubSection";
