10683a67a6816b09e0b00b60fbe99e46c3760b36
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-10 12:11:42 +0100

    FakeData dir moved, additional styling for CastCard

--- components/molecules/CastCard/CastCard.tsx
+++ components/molecules/CastCard/CastCard.tsx
@@ -1,12 +1,21 @@
-import { Container } from "./styles";
+import { Container, TextSection } from "./styles";
+
+import { Links } from "utils";
 import { ReactElement } from "react";
 
 interface ICastCardProps {}
 
 export default function CastCard({}: ICastCardProps): ReactElement {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Container
+      variant="vertical_image"
+      src={`${Links.tmdbImageURL}original/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg`}
+    >
+      <TextSection>
+        <span>Name</span>
+        <span>Played</span>
+        <span>Played</span>
+      </TextSection>
+    </Container>
   );
 }
--- components/molecules/CastCard/styles.tsx
+++ components/molecules/CastCard/styles.tsx
@@ -1,5 +1,33 @@
 import Card from "components/atoms/Card";
 import styled from "styled-components";
 
-export const Container = styled(Card)``;
+export const Container = styled(Card)`
+  div:nth-child(2) {
+    width: 90%;
+  }
+  max-height: 360px;
+  height: 360px;
+`;
 Container.displayName = "Container";
+
+export const TextSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: start;
+  ${({ theme }) => theme.fonts.main.regular};
+
+  > span {
+    text-align: start;
+  }
+
+  > span:first-child {
+    font-weight: bold;
+    margin: 0 0 0.25em 0;
+  }
+
+  > span:not(:first-child) {
+    font-weight: 300;
+  }
+`;
+TextSection.displayName = "TextSection";
