83dd0b75eaf6fc68ec29ec3c9f6d294c366cb65b
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-10 14:55:55 +0100

    MediaCard styling complete

--- components/molecules/MediaCard/MediaCard.tsx
+++ components/molecules/MediaCard/MediaCard.tsx
@@ -1,11 +1,22 @@
+import { FatContainer, TitleText } from "./styles";
+
+import Links from "utils/Links";
 import { ReactElement } from "react";
 
-interface IMediaCardProps {}
+type Variant = "Fat" | "Long";
+
+interface IMediaCardProps {
+  variant: Variant;
+}
 
 export default function MediaCard({}: IMediaCardProps): ReactElement {
   return (
-    <div>
-      <div></div>
-    </div>
+    <FatContainer
+      variant="vertical_image"
+      src={`${Links.youtubeThumbnailURL}GfbNLLcrItI/hqdefault.jpg`}
+      imageWidth={500}
+    >
+      <TitleText>Title</TitleText>
+    </FatContainer>
   );
 }
--- components/molecules/MediaCard/styles.tsx
+++ components/molecules/MediaCard/styles.tsx
@@ -1,1 +1,28 @@
+import Card from "components/atoms/Card";
 import styled from "styled-components";
+
+export const FatContainer = styled(Card)`
+  width: 500px;
+  max-width: 500px;
+  height: 436px;
+  max-height: 436px;
+
+  > div:first-child {
+    max-width: 500px;
+    max-height: 350px;
+  }
+
+  > div:last-child {
+    width: 100%;
+  }
+`;
+FatContainer.displayName = "FatContainer";
+
+export const TitleText = styled.p`
+  text-align: start;
+  width: 95%;
+  ${({ theme }) => theme.fonts.main.bigger};
+  margin: 0;
+  font-weight: bold;
+`;
+TitleText.displayName = "TitleText";
