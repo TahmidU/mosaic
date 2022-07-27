55c712c7c78915f5e8dea9f22d0d6bf6ed5bec13
TahmidU <tahmid.uddin.dev@gmail.com>
2022-07-27 15:55:30 +0100

    SearchPage initial creation

---
+++ components/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface ISearchPageProps {}
+
+export default function SearchPage({}: ISearchPageProps): ReactElement {
+  return (
+    <div>
+      <div>Test</div>
+    </div>
+  );
+}
