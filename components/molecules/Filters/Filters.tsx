7bcc2709d9e5654b3aeb93785ccf3b696b409a81
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-01 12:56:29 +0100

    Filters initial creation

---
+++ components/molecules/Filters/Filters.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface IFiltersProps {}
+
+export default function Filters({}: IFiltersProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
