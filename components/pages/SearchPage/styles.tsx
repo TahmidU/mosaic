cf955298aae6d9740e84605abcb0244325a55646
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-02 14:57:21 +0100

    Additional styling to SearchPage

--- components/pages/SearchPage/styles.tsx
+++ components/pages/SearchPage/styles.tsx
@@ -13,7 +13,13 @@ export const Header = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   width: 100%;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column-reverse;
+    align-items: unset;
+  }
 
   > span:first-child {
     ${({ theme }) => theme.fonts.main.medium};
@@ -23,5 +29,12 @@ Header.displayName = "Header";
 
 export const FiltersStyle = styled(Filters)`
   width: fit-content;
+
+  @media only screen and (max-width: 768px) {
+    margin: 0 0 1rem 0;
+    > span:first-child {
+      margin: 0;
+    }
+  }
 `;
 FiltersStyle.displayName = "FiltersStyle";
