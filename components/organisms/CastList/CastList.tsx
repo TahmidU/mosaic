a0cf2b1bd36752157cc1de7c11310d0d131eea47
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-10 17:49:23 +0100

    CastList component initial creation

---
+++ components/organisms/CastList/CastList.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface ICastListProps {}
+
+export default function CastList({}: ICastListProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
