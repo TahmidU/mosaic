8a83a3cd1667845a9c556d4502e8cb398683ec38
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-10 18:48:38 +0100

    MediaList complete

--- components/organisms/MediaList/MediaList.tsx
+++ components/organisms/MediaList/MediaList.tsx
@@ -1,11 +1,41 @@
-import { ReactElement } from "react";
+import { Dispatch, ReactElement, SetStateAction } from "react";
+import {
+  MediaCardStyle,
+  MediaHorizontalList,
+  MediaListWrapper,
+} from "./styles";
 
-interface IMediaListProps {}
+import { IVideo } from "types/api/videos";
+import { Links } from "utils";
 
-export default function MediaList({}: IMediaListProps): ReactElement {
+interface IMediaListProps {
+  videoData?: IVideo;
+  setModal: Dispatch<
+    SetStateAction<{
+      open: boolean;
+      initialIndex: number;
+    }>
+  >;
+  className?: string;
+}
+
+export default function MediaList({
+  videoData,
+  setModal,
+  className,
+}: IMediaListProps): ReactElement {
   return (
-    <div>
-      <div></div>
-    </div>
+    <MediaHorizontalList title="Media" className={className}>
+      {videoData &&
+        videoData.results.map((_video, index) => (
+          <MediaListWrapper key={index}>
+            <MediaCardStyle
+              title={_video.name}
+              src={`${Links.youtubeThumbnailURL}${_video.key}/hqdefault.jpg`}
+              onClick={() => setModal({ open: true, initialIndex: index })}
+            />
+          </MediaListWrapper>
+        ))}
+    </MediaHorizontalList>
   );
 }
