af69d191387e4e40503d555d11d898204d597082
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-10 18:31:41 +0100

    MediaList initial creation

---
+++ components/organisms/MediaList/MediaList.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface IMediaListProps {}
+
+export default function MediaList({}: IMediaListProps): ReactElement {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
