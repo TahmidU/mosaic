edecde60970ef11608502d4663eab6b3f8b85903
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-14 14:16:06 +0100

    key prop removed from ExternalLink.tsx

--- components/atoms/ExternalLink/ExternalLink.tsx
+++ components/atoms/ExternalLink/ExternalLink.tsx
@@ -3,16 +3,14 @@ import { ReactElement, ReactNode } from "react";
 interface IExternalLinkProps {
   href?: string;
   children?: ReactNode;
-  key?: number;
 }
 
 export default function ExternalLink({
   href,
   children,
-  key,
 }: IExternalLinkProps): ReactElement {
   return (
-    <a key={key} target="_blank" href={href} rel="noopener noreferrer">
+    <a target="_blank" href={href} rel="noopener noreferrer">
       {children}
     </a>
   );
