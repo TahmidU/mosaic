41dd5847da400916987c931f59a3b3bbb040b986
TahmidU <tahmid.uddin.dev@gmail.com>
2022-08-23 19:18:09 +0100

    CastCard refactored

--- components/molecules/CastCard/styles.tsx
+++ components/molecules/CastCard/styles.tsx
@@ -1,24 +1,34 @@
 import Card from "components/atoms/Card";
+import Image from "next/image";
 import styled from "styled-components";
 
 export const Container = styled(Card)`
-  > div:first-child {
-    max-width: 150px;
-    max-height: 225px;
-  }
-
-  div:nth-child(2) {
-    width: 90%;
-  }
   max-height: 360px;
   height: 360px;
 `;
 Container.displayName = "Container";
 
+export const ImageWrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  position: relative;
+  margin: 0 0 1rem 0;
+  max-width: 150px;
+  max-height: 225px;
+`;
+ImageWrapper.displayName = "ImageWrapper";
+
+export const CastImage = styled(Image)`
+  width: 100%;
+  height: 100%;
+  border-radius: 14px;
+`;
+CastImage.displayName = "CastImage";
+
 export const TextSection = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 90%;
   justify-content: start;
   ${({ theme }) => theme.fonts.main.regular};
 
