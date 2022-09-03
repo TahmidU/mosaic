ddebaf4f642b48b6a6a4a06711a922401fd223fe
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-03 16:21:06 +0100

    Checkbox test created

---
+++ components/atoms/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,12 @@
+import { cleanup, render, screen } from "utils/test-config";
+
+import Checkbox from ".";
+import React from "react";
+import getTheme from "resources/themes";
+import { removeAllSpaces } from "utils/TestUtils";
+
+afterEach(cleanup);
+
+describe("Checkbox", () => {
+  test("Test", () => {});
+});
