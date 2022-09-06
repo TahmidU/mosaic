83c309294352f3ba1c69356e4ca92132de1cc59e
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-06 13:00:15 +0100

    CastCard.test.tsx created

---
+++ components/molecules/CastCard/CastCard.test.tsx
@@ -0,0 +1,10 @@
+import { cleanup, render, screen } from "utils/test-config";
+
+import CastCard from "./CastCard";
+import React from "react";
+
+afterEach(cleanup);
+
+describe("CastCard", () => {
+  test("foo", () => {});
+});
