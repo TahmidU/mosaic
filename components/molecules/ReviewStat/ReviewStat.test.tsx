ec8d06e253031a98c1348590c05551000dcd0f4f
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-07 21:09:02 +0100

    ReviewStat test clean-up. Style test code improved

--- components/molecules/ReviewStat/ReviewStat.test.tsx
+++ components/molecules/ReviewStat/ReviewStat.test.tsx
@@ -6,67 +6,58 @@ import React from "react";
 import ReviewStat from "./ReviewStat";
 import TestThemeProvider from "components/atoms/Helper/TestThemeProvider";
 import getTheme from "resources/themes";
+import { removeAllSpaces } from "utils/TestUtils";
 import renderer from "react-test-renderer";
 
 afterEach(cleanup);
 
 describe("ReviewStat", () => {
+  const lightTheme = getTheme("light");
+
   test("Review 0%", () => {
-    // Given When
     const review = 0;
+
     const { getByText } = render(<ReviewStat percentage={review} />);
 
-    // Then
     getByText(`${review}%`);
   });
 
   test("Review 90% non-wait", () => {
-    // Given When
     const review = 90;
+
     const { queryByText } = render(<ReviewStat percentage={review} />);
 
-    // Then
     expect(queryByText(`${review}%`)).toBe(null);
   });
 
   test("Review 90%", async () => {
-    // Given When
     const review = 90;
+
     const { getByText } = render(<ReviewStat percentage={review} />);
 
-    // Then
     await new Promise((r) => setTimeout(r, 1400));
     getByText(`${review}%`);
   });
 
   test("Default light theme styles applied", async () => {
-    // Given
-    const lightTheme = getTheme("light");
     const review = 10;
 
-    // When
+    const expectedStyle = `
+      background-color: ${removeAllSpaces(lightTheme.almostBlack)};
+      border-radius: 100%;
+      border: 1px solid ${removeAllSpaces(lightTheme.almostBlack)};
+    `;
+
     const { getByTestId } = render(<ReviewStat percentage={review} />);
 
-    // Then
-    expect(getByTestId("ReviewStatContainer")).toHaveStyleRule(
-      "background-color",
-      lightTheme.almostBlack
-    );
-    expect(getByTestId("ReviewStatContainer")).toHaveStyleRule(
-      "border-radius",
-      "100%"
-    );
-    expect(getByTestId("ReviewStatContainer")).toHaveStyleRule(
-      "border",
-      `1px solid ${lightTheme.almostBlack}`
-    );
+    const reviewContainer = getByTestId("ReviewStatContainer");
+
+    expect(reviewContainer).toHaveStyle(expectedStyle);
   });
 
   test("Snapshot", () => {
-    // Given
     const review = 10;
 
-    // When
     const rendered = renderer.create(
       <TestThemeProvider>
         <ReviewStat percentage={review} />
@@ -74,7 +65,6 @@ describe("ReviewStat", () => {
     );
     const tree = rendered.toJSON();
 
-    // Then
     expect(tree).toMatchSnapshot();
   });
 });
