a5e52e4ffcfb1bed4b16570878720d00e9e33f1c
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-09 19:23:57 +0100

    Filter.test.tsx created

---
+++ components/molecules/Filters/Filter.test.tsx
@@ -0,0 +1,12 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import React from "react";
+import Filter from "./Filters";
+
+afterEach(cleanup);
+
+describe("Filter", () => {
+  test("TypeFilter, initial state", () => {});
+
+  test("TypeFilter, select TV type", () => {});
+});
