7db5bf97b22a0611461fe25b5d4d0297ab3b48cb
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-11 18:02:55 +0100

    HorizontalList.test.tsx created

---
+++ components/molecules/HorizontalList/HorizontalList.test.tsx
@@ -0,0 +1,15 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+afterEach(cleanup);
+
+describe("HorizontalList", () => {
+  test("Title props", () => {});
+
+  test("Child appearance", () => {});
+
+  test("Loading elements appearance", () => {});
+
+  test("Next button click", () => {});
+
+  test("Previous button click", () => {});
+});
