b0b32e4b3e76e87cdc98ad5bae1f30b3cfc57966
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-13 18:50:48 +0100

    Social.tsx and ExternalLink.tsx testId prop changes. Socials.test.tsx created

--- components/atoms/ExternalLink/ExternalLink.tsx
+++ components/atoms/ExternalLink/ExternalLink.tsx
@@ -3,14 +3,21 @@ import { ReactElement, ReactNode } from "react";
 interface IExternalLinkProps {
   href?: string;
   children?: ReactNode;
+  testId?: string;
 }
 
 export default function ExternalLink({
   href,
   children,
+  testId = "ExternalLink",
 }: IExternalLinkProps): ReactElement {
   return (
-    <a target="_blank" href={href} rel="noopener noreferrer">
+    <a
+      data-testid={testId}
+      target="_blank"
+      href={href}
+      rel="noopener noreferrer"
+    >
       {children}
     </a>
   );
---
+++ components/molecules/Socials/Socials.test.tsx
@@ -0,0 +1,11 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import React from "react";
+import Socials from "./Socials";
+import { removeAllSpaces } from "utils/TestUtils";
+
+afterEach(cleanup);
+
+describe("Socials", () => {
+  test("Filter socials", () => {});
+});
