4031060857097538af644b72110c9c144b16b22b
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-13 18:30:56 +0100

    SlideButton.test.tsx created

---
+++ components/molecules/SlideButton/SlideButton.test.tsx
@@ -0,0 +1,13 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import React from "react";
+import SlideButton from "./SlideButton";
+import { removeAllSpaces } from "utils/TestUtils";
+
+afterEach(cleanup);
+
+describe("SlideButton", () => {
+  test("onClick", () => {});
+
+  test("Default variant, animation style", () => {});
+});
