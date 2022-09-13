c8c7848fbf21bab9509c1c48d9e6e2c7d5a7f3d5
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-13 14:49:36 +0100

    Title style test complete

--- components/molecules/MovieCard/MovieCard.test.tsx
+++ components/molecules/MovieCard/MovieCard.test.tsx
@@ -2,11 +2,31 @@ import { cleanup, fireEvent, render, screen } from "utils/test-config";
 
 import MovieCard from "./MovieCard";
 import React from "react";
+import getTheme from "resources/themes";
+import { removeAllSpaces } from "utils/TestUtils";
 
 afterEach(cleanup);
 
 describe("MovieCard", () => {
-  test("Title style", () => {});
+  const lightTheme = getTheme("light");
+  test("Title style", () => {
+    const expectedTitle = "NewTitle";
+    const expectedTitleStyle = `
+        font-weight: 600;
+        ${removeAllSpaces(lightTheme.fonts.tertiary.regular)}
+    `;
+
+    render(
+      <MovieCard
+        movieTitle={expectedTitle}
+        src="https://via.placeholder.com/220x330/000000?text=FooBar"
+        review={5.0}
+        movieReleaseDate={"2022/01/01"}
+      />
+    );
+
+    expect(screen.getByText(expectedTitle)).toHaveStyle(expectedTitleStyle);
+  });
 
   test("Description style", () => {});
 
