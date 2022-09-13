0c068c3cf9bb69879cddf91e9479eb63dbdbfac8
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-13 14:40:58 +0100

    MovieCard.test.tsx created

---
+++ components/molecules/MovieCard/MovieCard.test.tsx
@@ -0,0 +1,12 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import MovieCard from "./MovieCard";
+import React from "react";
+
+afterEach(cleanup);
+
+describe("MovieCard", () => {
+  test("Title, description and release date", () => {});
+
+  test("onClick", () => {});
+});
