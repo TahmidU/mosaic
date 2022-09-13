57ed52bf21650c5322abd6e6b7c49c98da70197d
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-13 15:49:07 +0100

    SearchCard.test.tsx created

---
+++ components/molecules/SearchCard/SearchCard.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import SearchCard from "./SearchCard";
+import React from "react";
+import getTheme from "resources/themes";
+
+afterEach(cleanup);
+
+describe("SearchCard", () => {
+  const lightTheme = getTheme("light");
+  test("Title style", () => {});
+
+  test("Review style", () => {});
+
+  test("Release date style", () => {});
+});
