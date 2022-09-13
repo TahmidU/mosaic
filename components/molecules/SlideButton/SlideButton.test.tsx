6a0be1d7cb6df05a4f1ba13647cea936a6c1cce2
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-13 18:39:12 +0100

    onClick test for all variants

--- components/molecules/SlideButton/SlideButton.test.tsx
+++ components/molecules/SlideButton/SlideButton.test.tsx
@@ -7,7 +7,45 @@ import { removeAllSpaces } from "utils/TestUtils";
 afterEach(cleanup);
 
 describe("SlideButton", () => {
-  test("onClick", () => {});
+  test("Default variant onClick", () => {
+    const onClickMock = jest.fn();
+    const slideButtonId = "SlideButton";
+
+    render(<SlideButton onClick={onClickMock} />);
+
+    fireEvent.click(screen.getByTestId(slideButtonId));
+    expect(onClickMock).toBeCalledTimes(1);
+  });
+
+  test("circleSimple variant onClick", () => {
+    const onClickMock = jest.fn();
+    const slideButtonId = "SlideButton";
+
+    render(<SlideButton onClick={onClickMock} variant="circleSimpleLeft" />);
+
+    fireEvent.click(screen.getByTestId(slideButtonId));
+    expect(onClickMock).toBeCalledTimes(1);
+  });
+
+  test("transparent variant onClick", () => {
+    const onClickMock = jest.fn();
+    const slideButtonId = "SlideButton";
+
+    render(<SlideButton onClick={onClickMock} variant="transparentLeft" />);
+
+    fireEvent.click(screen.getByTestId(slideButtonId));
+    expect(onClickMock).toBeCalledTimes(1);
+  });
+
+  test("simple variant onClick", () => {
+    const onClickMock = jest.fn();
+    const slideButtonId = "SlideButton";
+
+    render(<SlideButton onClick={onClickMock} variant="simpleLeft" />);
+
+    fireEvent.click(screen.getByTestId(slideButtonId));
+    expect(onClickMock).toBeCalledTimes(1);
+  });
 
   test("Default variant, animation style", () => {});
 });
