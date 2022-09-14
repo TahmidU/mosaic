48e2f4225df6740de21efad3b1acb8e801643a49
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-14 16:38:28 +0100

    StarRating.test.tsx created

---
+++ components/molecules/StarRating/StarRating.test.tsx
@@ -0,0 +1,12 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import React from "react";
+import StarRating from "./StarRating";
+
+afterEach(cleanup);
+
+describe("StarRating", () => {
+  test("Stars gradient", () => {});
+
+  test("Rating text", () => {});
+});
