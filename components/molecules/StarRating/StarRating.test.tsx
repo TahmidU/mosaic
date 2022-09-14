def71e67a94e48027c0f12c1429f182bc59b8182
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-14 16:49:08 +0100

    StarRating.test.tsx tests complete + clean-up

--- components/molecules/StarRating/StarRating.test.tsx
+++ components/molecules/StarRating/StarRating.test.tsx
@@ -1,12 +1,31 @@
-import { cleanup, fireEvent, render, screen } from "utils/test-config";
+import { cleanup, render, screen } from "utils/test-config";
 
 import React from "react";
 import StarRating from "./StarRating";
+import getTheme from "resources/themes";
+import { removeAllSpaces } from "utils/TestUtils";
 
 afterEach(cleanup);
 
 describe("StarRating", () => {
-  test("Stars gradient", () => {});
+  const lightTheme = getTheme("light");
 
-  test("Rating text", () => {});
+  test("Stars gradient and rating text", () => {
+    const rating = 5.0;
+    const expectedGradientId = "StarRating-StarsGradient";
+    const expectedRating = `${((rating / 10) * 5).toFixed(2)}/5`;
+    const expectedGradientStyle = `
+        background: linear-gradient(to right,#FD1C1C ${rating}%,transparent ${rating}%);
+    `;
+    const expectedReviewStyle = `
+        ${removeAllSpaces(lightTheme.fonts.main.regular)}
+    `;
+
+    render(<StarRating rating={5.0} />);
+
+    expect(screen.getByTestId(expectedGradientId)).toHaveStyle(
+      expectedGradientStyle
+    );
+    expect(screen.getByText(expectedRating)).toHaveStyle(expectedReviewStyle);
+  });
 });
