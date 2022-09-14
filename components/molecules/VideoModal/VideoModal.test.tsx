9ba20b36fca2c2f956e804a9985b6da0a3a2502e
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-14 17:03:30 +0100

    VideoModal.test.tsx created

---
+++ components/molecules/VideoModal/VideoModal.test.tsx
@@ -0,0 +1,16 @@
+import { cleanup, render, screen } from "utils/test-config";
+
+import React from "react";
+import VideoModal from "./VideoModal";
+
+afterEach(cleanup);
+
+describe("VideoModal", () => {
+  test("Close button test", () => {});
+
+  test("Next button", () => {});
+
+  test("Previous button", () => {});
+
+  test("Video div appearance", () => {});
+});
