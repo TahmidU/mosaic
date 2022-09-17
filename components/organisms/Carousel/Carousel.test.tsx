22101d909ebe73e52781ad77337720f160561377
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-17 17:28:53 +0100

    Mobile Carousel test complete

--- components/organisms/Carousel/Carousel.test.tsx
+++ components/organisms/Carousel/Carousel.test.tsx
@@ -124,4 +124,53 @@ describe("Mobile Carousel", () => {
     screen.getByText(expectedDesc);
     screen.getByText(expectedDate, { exact: false });
   });
+
+  test("Mobile Correct number of StepStatus components", async () => {
+    const data = FakeDiscoverMovie;
+
+    render(<Carousel carouselData={data} desktopDisableAutoSlide />);
+
+    await new Promise((r) => setTimeout(r, 1000));
+    expect(screen.getAllByTestId("StepStatusIndicator").length).toBe(3);
+  });
+
+  test("Mobile Click prev from start", async () => {
+    const data = FakeDiscoverMovie,
+      target = data[2],
+      expectedTitle = target.title;
+
+    render(<Carousel carouselData={data} desktopDisableAutoSlide />);
+
+    fireEvent.click(screen.getByTestId("MobileCarouselPrevBtn"));
+    await new Promise((r) => setTimeout(r, 1000));
+    screen.getByText(expectedTitle);
+  });
+
+  test("Mobile Click next from end", async () => {
+    const data = FakeDiscoverMovie,
+      target = data[0],
+      expectedTitle = target.title;
+
+    render(<Carousel carouselData={data} desktopDisableAutoSlide />);
+
+    const nextBtn = screen.getByTestId("MobileCarouselNextBtn");
+    for (let i = 0; i < data.length; i++) {
+      fireEvent.click(nextBtn);
+    }
+
+    await new Promise((r) => setTimeout(r, 1000));
+    screen.getByText(expectedTitle);
+  });
+
+  test("Mobile Click StepStatus to change slide", async () => {
+    const data = FakeDiscoverMovie,
+      target = data[1],
+      expectedTitle = target.title;
+
+    render(<Carousel carouselData={data} desktopDisableAutoSlide />);
+
+    fireEvent.click(screen.getAllByTestId("StepStatusIndicator")[1]);
+    await new Promise((r) => setTimeout(r, 1000));
+    screen.getByText(expectedTitle);
+  });
 });
