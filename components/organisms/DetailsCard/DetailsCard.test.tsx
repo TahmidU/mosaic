5d98d1a488dfa686dbd3848d2e9d7d8287ac5f6e
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-18 16:05:42 +0100

    Tests setup

--- components/organisms/DetailsCard/DetailsCard.test.tsx
+++ components/organisms/DetailsCard/DetailsCard.test.tsx
@@ -5,4 +5,14 @@ import React from "react";
 
 afterEach(cleanup);
 
-describe("DetailsCard", () => {});
+describe("DetailsCard", () => {
+  test("Title, release date, genres, runtime, and language", () => {});
+
+  test("Additional details movie", () => {});
+
+  test("Additional details TV", () => {});
+
+  test("Providers movie", () => {});
+
+  test("Providers TV", () => {});
+});
