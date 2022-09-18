d0b2fac63b9cc209701a5f78ad923bace8d81321
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-18 15:59:20 +0100

    DetailsCard.test.tsx created

---
+++ components/organisms/DetailsCard/DetailsCard.test.tsx
@@ -0,0 +1,8 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import DetailsCard from "./DetailsCard";
+import React from "react";
+
+afterEach(cleanup);
+
+describe("DetailsCard", () => {});
