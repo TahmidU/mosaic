bcc5406414562eb82cc294ca0fdacac36b54866e
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-18 16:33:52 +0100

    Release date for movie and tv tests complete

--- components/organisms/DetailsCard/DetailsCard.test.tsx
+++ components/organisms/DetailsCard/DetailsCard.test.tsx
@@ -4,6 +4,7 @@ import DetailsCard from "./DetailsCard";
 import { IMovieDetails } from "types/movie";
 import { ITVDetails } from "types/tv";
 import React from "react";
+import { TextUtils } from "utils";
 import { fakeMovieData } from "resources/TestResources/MovieDetails";
 import { fakeTVData } from "resources/TestResources/TVDetails";
 
@@ -28,7 +29,7 @@ describe("DetailsCard", () => {
 
     render(
       <DetailsCard
-        mediaType="movie"
+        mediaType="tv"
         details={fakeTVData as IMovieDetails & ITVDetails}
       />
     );
@@ -36,7 +37,35 @@ describe("DetailsCard", () => {
     screen.getByText(expectedTitle);
   });
 
-  test("Release date, genres, runtime, and language", () => {});
+  test("Release date movie", () => {
+    const date = fakeMovieData.release_date;
+    const expectedDate = TextUtils.dateFormatter(date) as string;
+
+    render(
+      <DetailsCard
+        mediaType="movie"
+        details={fakeMovieData as IMovieDetails & ITVDetails}
+      />
+    );
+
+    screen.getByText(expectedDate, { exact: false, selector: "span" });
+  });
+
+  test("Release date TV", () => {
+    const date = fakeTVData.first_air_date;
+    const expectedDate = TextUtils.dateFormatter(date) as string;
+
+    render(
+      <DetailsCard
+        mediaType="tv"
+        details={fakeTVData as IMovieDetails & ITVDetails}
+      />
+    );
+
+    screen.getByText(expectedDate, { exact: false, selector: "span" });
+  });
+
+  test("Genres, runtime, and language", () => {});
 
   test("Additional details movie", () => {});
 
