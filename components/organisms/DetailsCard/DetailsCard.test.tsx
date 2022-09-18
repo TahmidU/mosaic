41d2f30d0b55c45518941abb85ef99aa1da62dd6
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-18 16:51:52 +0100

    Genres, runtime and language test created

--- components/organisms/DetailsCard/DetailsCard.test.tsx
+++ components/organisms/DetailsCard/DetailsCard.test.tsx
@@ -1,10 +1,10 @@
+import { MathUtils, TextUtils } from "utils";
 import { cleanup, fireEvent, render, screen } from "utils/test-config";
 
 import DetailsCard from "./DetailsCard";
 import { IMovieDetails } from "types/movie";
 import { ITVDetails } from "types/tv";
 import React from "react";
-import { TextUtils } from "utils";
 import { fakeMovieData } from "resources/TestResources/MovieDetails";
 import { fakeTVData } from "resources/TestResources/TVDetails";
 
@@ -65,7 +65,24 @@ describe("DetailsCard", () => {
     screen.getByText(expectedDate, { exact: false, selector: "span" });
   });
 
-  test("Genres, runtime, and language", () => {});
+  test("Genres, runtime, and language", () => {
+    const expectedGenres = fakeMovieData.genres
+      .map((_genre) => _genre.name)
+      .join(", ");
+    const expectedRuntime = MathUtils.shortenRuntime(fakeMovieData.runtime);
+    const expectedLanguage = fakeMovieData.original_language;
+
+    render(
+      <DetailsCard
+        mediaType="movie"
+        details={fakeMovieData as IMovieDetails & ITVDetails}
+      />
+    );
+
+    screen.getByText(expectedGenres, { exact: false, selector: "span" });
+    screen.getByText(`${expectedRuntime}`, { exact: false, selector: "span" });
+    screen.getByText(expectedLanguage, { exact: true, selector: "span" });
+  });
 
   test("Additional details movie", () => {});
 
