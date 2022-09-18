2345311b025a440ad75dba6b277c7b277a1898d6
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-18 19:04:16 +0100

    SearchInput.test.tsx created

---
+++ components/organisms/SearchInput/SearchInput.test.tsx
@@ -0,0 +1,10 @@
+import { cleanup, fireEvent, render, screen } from "utils/test-config";
+
+import SearchInput from "./SearchInput";
+import React from "react";
+
+afterEach(cleanup);
+
+describe("SearchInput", () => {});
+
+describe("MobileSearchInput", () => {});
