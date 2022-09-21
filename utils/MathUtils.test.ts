9b8b982fe12ec5d5adf854a7b9e582908699fff8
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-21 16:34:09 +0100

    Modular cycle test complete

--- utils/MathUtils.test.ts
+++ utils/MathUtils.test.ts
@@ -1,7 +1,29 @@
 import { MathUtils } from ".";
 
 describe("MathUtils", () => {
-  describe("modCycle", () => {});
+  describe("modCycle", () => {
+    test("Modular cycle", () => {
+      const expectedIteration: { [key: number]: number } = {
+        0: 0,
+        1: 1,
+        2: 2,
+        3: 0,
+        4: 1,
+        5: 2,
+        6: 0,
+        7: 1,
+        8: 2,
+        9: 0,
+      };
+      const mod = 3;
+      const range = 10;
+
+      for (let i = 0; i < range; i++) {
+        const result = MathUtils.modCycle(i, mod);
+        expect(result).toEqual(expectedIteration[i]);
+      }
+    });
+  });
 
   describe("shortenRuntime", () => {});
 });
