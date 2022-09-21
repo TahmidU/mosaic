350bef5c15c8a307cb3cafcd7a667803de7c0fa7
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-21 17:07:13 +0100

    TestUtils tests complete

--- utils/TestUtils.test.ts
+++ utils/TestUtils.test.ts
@@ -1,5 +1,22 @@
 import { TestUtils } from ".";
 
 describe("TestUtils", () => {
-  describe("removeAllSpaces", () => {});
+  describe("removeAllSpaces", () => {
+    test("Space removal", () => {
+      const str = "h e l lo";
+      const expectedStr = "hello";
+
+      const result = TestUtils.removeAllSpaces(str);
+
+      expect(result).toEqual(expectedStr);
+    });
+
+    test("No space removal", () => {
+      const expectedStr = "hello";
+
+      const result = TestUtils.removeAllSpaces(expectedStr);
+
+      expect(result).toEqual(expectedStr);
+    });
+  });
 });
