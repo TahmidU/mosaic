dc61cfc747b407c9e633d57f3b6c36439214793a
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-22 18:22:35 +0100

    FakeVideos test resource added

--- components/molecules/VideoModal/VideoModal.stories.tsx
+++ components/molecules/VideoModal/VideoModal.stories.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { fakeMovieData } from "resources/TestResources/MovieDetails";
+import { FakeVideos } from "resources/TestResources/Videos";
 import { IPayload } from "types/story";
 import VideoModal from "./VideoModal";
 
@@ -7,7 +10,22 @@ const payload: IPayload = {
 };
 export default payload;
 
-export const Basic = (args: any) => <VideoModal {...args} />;
+export const Basic = (args: any) => {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <div style={{ width: "100wv", height: "100vh" }}>
+      <button onClick={() => setOpen(true)}>Open</button>
+      <div id="modalPortal" style={{ width: "100wv", height: "100vh" }}></div>
+
+      <VideoModal
+        videos={FakeVideos}
+        modalOpen={isOpen}
+        setModalOpen={setOpen}
+        {...args}
+      />
+    </div>
+  );
+};
 Basic.args = {
   open: true,
   onClose: () => {},
