53cef17d13845da4677be229d2fa94046bf040d7
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-22 17:55:54 +0100

    HorizontalList story more interactable

--- components/molecules/HorizontalList/HorizontalList.stories.tsx
+++ components/molecules/HorizontalList/HorizontalList.stories.tsx
@@ -1,5 +1,8 @@
 import HorizontalList from "./HorizontalList";
 import { IPayload } from "types/story";
+import { useState } from "react";
+import styled from "styled-components";
+import Card from "components/atoms/Card";
 
 const payload: IPayload = {
   title: "MOLECULES/HorizontalList",
@@ -13,17 +16,56 @@ enum RandomEnum {
   FOOBAR = "FooBar",
 }
 
+export const CardWrapper = styled.div`
+  display: inline-block;
+  width: calc((100%) / 5);
+  > div {
+    margin: 0 auto;
+  }
+
+  @media only screen and (max-width: 912px) {
+    width: calc((100%) / 4);
+  }
+
+  @media only screen and (max-width: 748px) {
+    width: calc((100%) / 3);
+  }
+
+  @media only screen and (max-width: 568px) {
+    width: calc((100%) / 2);
+  }
+
+  @media only screen and (max-width: 360px) {
+    width: calc((100%) / 1);
+  }
+`;
+
 export const Basic = (args: any) => {
+  const [child, setChild] = useState("Foo");
+
   return (
     <HorizontalList
       title="Test"
       subListTitles={[RandomEnum.FOO, RandomEnum.BAR, RandomEnum.FOOBAR]}
+      onSubTitleClick={(title) => {
+        setChild(title);
+      }}
+      loadingElements={<span>loading...</span>}
       {...args}
     >
-      <p>Test</p>
+      {Array.from(Array(10).keys()).map((_item, index) => (
+        <CardWrapper key={index}>
+          <Card>{child}</Card>
+        </CardWrapper>
+      ))}
     </HorizontalList>
   );
 };
 Basic.args = {
   loading: false,
+};
+Basic.parameters = {
+  controls: {
+    include: ["loading", "title"],
+  },
 };
