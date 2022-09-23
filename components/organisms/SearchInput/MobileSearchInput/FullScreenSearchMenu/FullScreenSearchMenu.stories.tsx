ede5f77d0ae66c8d6ccad9876c8a91adc61f58e2
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-23 18:23:33 +0100

    FullScreenSearchMenu story working

--- components/organisms/SearchInput/MobileSearchInput/FullScreenSearchMenu/FullScreenSearchMenu.stories.tsx
+++ components/organisms/SearchInput/MobileSearchInput/FullScreenSearchMenu/FullScreenSearchMenu.stories.tsx
@@ -1,5 +1,6 @@
 import FullScreenSearchMenu from "./FullScreenSearchMenu";
 import { IPayload } from "types/story";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 import { useState } from "react";
 
 const payload: IPayload = {
@@ -8,17 +9,27 @@ const payload: IPayload = {
 };
 export default payload;
 
+const router: any = {
+  route: "/search",
+  pathname: "/search",
+  query: { type: "movie" },
+  asPath: "/search",
+  basePath: "/search",
+};
+
 export const Basic = (args: any) => {
   const [isOpen, setOpen] = useState(false);
   return (
-    <div>
-      <button onClick={() => setOpen(true)}>Open</button>
-      <FullScreenSearchMenu
-        isMenuOpen={isOpen}
-        setMenuOpen={setOpen}
-        {...args}
-      />
-    </div>
+    <RouterContext.Provider value={router}>
+      <div>
+        <button onClick={() => setOpen(true)}>Open</button>
+        <FullScreenSearchMenu
+          isMenuOpen={isOpen}
+          setMenuOpen={setOpen}
+          {...args}
+        />
+      </div>
+    </RouterContext.Provider>
   );
 };
 Basic.args = {};
