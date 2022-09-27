4c524b04ed46797f21656941a80f57c2114e7b31
TahmidU <tahmid.uddin.dev@gmail.com>
2022-09-27 17:36:52 +0100

    Jest and Cypress assertion conflicts resolved

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'cb3yfm',
+  projectId: "cb3yfm",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
