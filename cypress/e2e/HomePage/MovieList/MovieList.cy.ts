7cc0ab515d53a6ffcaa063058c50d1472f80a430
TahmidU <tahmid.uddin.dev@gmail.com>
2022-10-02 20:17:16 +0100

    MovieList - Next test working

--- cypress/e2e/HomePage/MovieList/MovieList.cy.ts
+++ cypress/e2e/HomePage/MovieList/MovieList.cy.ts
@@ -1,5 +1,40 @@
 describe("MovieList", () => {
-  it("Next", () => {});
+  it("Next", () => {
+    // Expects
+    const nextBtnId = '[data-testid="HomePageMovieList-RightBtn"]';
+    const listId = '[data-testid="HomePageMovieList-List"]';
+
+    // Setup
+    cy.viewport("macbook-16");
+    const getMoviesIntercept = "getMovies";
+
+    cy.intercept({
+      method: "GET",
+      url: "/api/movies/**",
+      hostname: "localhost",
+    }).as(getMoviesIntercept);
+
+    // Results
+    cy.visit("http://localhost:3000/");
+
+    cy.wait(`@${getMoviesIntercept}`);
+
+    cy.get(listId)
+      .children()
+      .then(($elms: any) => {
+        for (let i = 0; i < 5; i++) {
+          cy.get(`[href="${$elms[i].getAttribute("href")}"]`).should(
+            "be.visible"
+          );
+        }
+        cy.get(nextBtnId).click();
+        for (let i = 5; i < 11; i++) {
+          cy.get(`[href="${$elms[i].getAttribute("href")}"]`).should(
+            "be.visible"
+          );
+        }
+      });
+  });
 
   it("Prev", () => {});
 
