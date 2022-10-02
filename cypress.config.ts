0436813be551cf3e962b3bc6d8dfa81184c0d44b
TahmidU <tahmid.uddin.dev@gmail.com>
2022-10-02 19:36:01 +0100

    MovieList cypress setup. Husky configured to run Cypress on pre-commit. .gitignore configured.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "cb3yfm",
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
---
+++ cypress/e2e/HomePage/MovieList/MovieList.cy.ts
@@ -0,0 +1,13 @@
+describe("MovieList", () => {
+  it("Next", () => {});
+
+  it("Prev", () => {});
+
+  it("Next cycle back to start", () => {});
+
+  it("Prev cycle back to start", () => {});
+
+  it("Change type", () => {});
+});
+
+export {};
