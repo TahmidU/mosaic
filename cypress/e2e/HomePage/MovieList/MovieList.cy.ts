04745650f5372092ff3a4127100aa75a678e073a
TahmidU <tahmid.uddin.dev@gmail.com>
2022-10-07 18:52:07 +0100

    MovieList Prev cycle back to start test complete

--- cypress/e2e/HomePage/MovieList/MovieList.cy.ts
+++ cypress/e2e/HomePage/MovieList/MovieList.cy.ts
@@ -63,7 +63,7 @@ describe("MovieList", () => {
 
   it("Next cycle back to start", () => {
     // Expects
-    const expectedInitialScroll = 0;
+    const expectedScroll = 0;
     const nextBtnId = '[data-testid="HomePageMovieList-RightBtn"]';
     const listId = '[data-testid="HomePageMovieList-List"]';
 
@@ -85,7 +85,7 @@ describe("MovieList", () => {
 
     cy.get(listId)
       .then(($element) => {
-        expect($element[0].scrollLeft).to.equal(expectedInitialScroll);
+        expect($element[0].scrollLeft).to.equal(expectedScroll);
       })
       .then(() => {
         cy.get(listId).then(($element) => {
@@ -103,14 +103,58 @@ describe("MovieList", () => {
           }
 
           const currentScrollLeft = $element[0].scrollLeft;
-          expect(expectedInitialScroll).to.equal(currentScrollLeft);
+          expect(expectedScroll).to.equal(currentScrollLeft);
         });
       });
   });
 
-  /*
-  it("Prev cycle back to start", () => {});
+  it("Prev cycle back to start", () => {
+    // Expects
+    const expectedScroll = 0;
+    const prevBtnId = '[data-testid="HomePageMovieList-LeftBtn"]';
+    const listId = '[data-testid="HomePageMovieList-List"]';
 
+    // Setup
+    cy.viewport("macbook-16");
+    const getMoviesIntercept = "getMovies";
+
+    cy.intercept({
+      method: "GET",
+      url: "/api/movies/**",
+      hostname: "localhost",
+    }).as(getMoviesIntercept);
+
+    // Results
+    cy.visit("http://localhost:3000/");
+
+    cy.wait(`@${getMoviesIntercept}`);
+    cy.wait(1000); // Wait for elements in list to appear;
+
+    cy.get(listId)
+      .then(($element) => {
+        expect($element[0].scrollLeft).to.equal(expectedScroll);
+      })
+      .then(() => {
+        cy.get(listId).then(($element) => {
+          let scrollWidth = $element[0].scrollWidth;
+          let childrenWidth = $element.children()[0].offsetWidth;
+
+          const numOfClickRequired = Math.ceil(
+            scrollWidth / (childrenWidth * 8)
+          );
+          cy.log(numOfClickRequired + "");
+
+          for (let i = 0; i < numOfClickRequired; i++) {
+            cy.get(prevBtnId).click();
+            cy.wait(1000); // Wait for clicks to finish
+          }
+
+          const currentScrollLeft = $element[0].scrollLeft;
+          expect(expectedScroll).to.equal(currentScrollLeft);
+        });
+      });
+  });
+  /*
   it("Change type", () => {});*/
 });
 
