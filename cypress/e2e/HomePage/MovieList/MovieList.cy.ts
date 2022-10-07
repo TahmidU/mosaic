9b9728d8959e9c5dc90dc6f62d799946c2daabbe
TahmidU <tahmid.uddin.dev@gmail.com>
2022-10-07 18:47:48 +0100

    Current tests in MovieList fixed. Next cycle back to start test complete

--- cypress/e2e/HomePage/MovieList/MovieList.cy.ts
+++ cypress/e2e/HomePage/MovieList/MovieList.cy.ts
@@ -1,6 +1,7 @@
 describe("MovieList", () => {
   it("Next", () => {
     // Expects
+    const expectedScroll = 0;
     const nextBtnId = '[data-testid="HomePageMovieList-RightBtn"]';
     const listId = '[data-testid="HomePageMovieList-List"]';
 
@@ -20,21 +21,18 @@ describe("MovieList", () => {
     cy.wait(`@${getMoviesIntercept}`);
     cy.wait(1000); // Wait for elements in list to appear;
 
-    cy.get(listId).then(($element) => {
-      cy.wrap($element[0].scrollLeft).as("initialScroll");
-    });
-
     cy.get(nextBtnId).click();
     cy.wait(1000);
 
     cy.get(listId).then(($element) => {
       const currentScrollLeft = $element[0].scrollLeft;
-      expect(cy.get("@initialScroll")).to.not.equal(currentScrollLeft);
+      expect(expectedScroll).to.not.equal(currentScrollLeft);
     });
   });
 
   it("Prev", () => {
     // Expects
+    const expectedScroll = 0;
     const prevBtnId = '[data-testid="HomePageMovieList-LeftBtn"]';
     const listId = '[data-testid="HomePageMovieList-List"]';
 
@@ -54,22 +52,63 @@ describe("MovieList", () => {
     cy.wait(`@${getMoviesIntercept}`);
     cy.wait(1000); // Wait for elements in list to appear;
 
-    cy.get(listId).then(($element) => {
-      cy.wrap($element[0].scrollLeft).as("initialScroll");
-    });
-
     cy.get(prevBtnId).click();
     cy.wait(1000);
 
     cy.get(listId).then(($element) => {
       const currentScrollLeft = $element[0].scrollLeft;
-      expect(cy.get("@initialScroll")).to.not.equal(currentScrollLeft);
+      expect(expectedScroll).to.not.equal(currentScrollLeft);
     });
   });
 
-  /*
-  it("Next cycle back to start", () => {});
+  it("Next cycle back to start", () => {
+    // Expects
+    const expectedInitialScroll = 0;
+    const nextBtnId = '[data-testid="HomePageMovieList-RightBtn"]';
+    const listId = '[data-testid="HomePageMovieList-List"]';
 
+    // Setup
+    cy.viewport("macbook-16");
+    const getMoviesIntercept = "getMovies";
+
+    cy.intercept({
+      method: "GET",
+      url: "/api/movies/**",
+      hostname: "localhost",
+    }).as(getMoviesIntercept);
+
+    // Results
+    cy.visit("http://localhost:3000/");
+
+    cy.wait(`@${getMoviesIntercept}`);
+    cy.wait(1000); // Wait for elements in list to appear;
+
+    cy.get(listId)
+      .then(($element) => {
+        expect($element[0].scrollLeft).to.equal(expectedInitialScroll);
+      })
+      .then(() => {
+        cy.get(listId).then(($element) => {
+          let scrollWidth = $element[0].scrollWidth;
+          let childrenWidth = $element.children()[0].offsetWidth;
+
+          const numOfClickRequired = Math.ceil(
+            scrollWidth / (childrenWidth * 8)
+          );
+          cy.log(numOfClickRequired + "");
+
+          for (let i = 0; i < numOfClickRequired; i++) {
+            cy.get(nextBtnId).click();
+            cy.wait(1000); // Wait for clicks to finish
+          }
+
+          const currentScrollLeft = $element[0].scrollLeft;
+          expect(expectedInitialScroll).to.equal(currentScrollLeft);
+        });
+      });
+  });
+
+  /*
   it("Prev cycle back to start", () => {});
 
   it("Change type", () => {});*/
